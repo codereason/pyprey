f2a05ceace3dc992e1d3a95f5be27f0e13936334
codereason <sdu_huangyu@foxmail.com>
2020-12-10 11:50:11 +0800

    Finish crawlers for Zhihu self page activity.

--- crawlers/zhihu_activ/zhihu_activ/spiders/zhihu_self_act.py
+++ crawlers/zhihu_activ/zhihu_activ/spiders/zhihu_self_act.py
@@ -1,4 +1,4 @@
-#coding=utf-8
+# coding=utf-8
 from __future__ import absolute_import
 import scrapy
 from scrapy.linkextractors import LinkExtractor
@@ -13,12 +13,15 @@ class ZhihuSelfActSpider(CrawlSpider):
     ''' zhihu my own page activity'''
     name = 'zhihu_self_act'
     allowed_domains = ['zhihu.com']
-    start_urls = ['http://zhihu.com/people/lian-shang-tian-kong-lan']
 
     rules = (
         Rule(LinkExtractor(allow=r'Items/'),
              callback='parse_item', follow=True),
     )
+
+    def __init__(self):
+        self.user_name = 'lian-shang-tian-kong-lan'
+        self.zhihu_sess_id = '1271442717533786112'
 
     def googlebot_header(self):
         headers = {"User-agent": "Googlebot"}
@@ -27,17 +30,43 @@ class ZhihuSelfActSpider(CrawlSpider):
     def start_requests(self):
         print("starting req")
 
-
-
-        yield scrapy.http.Request("https://www.zhihu.com/people/lian-shang-tian-kong-lan/", headers=self.googlebot_header(),
+        yield scrapy.http.Request("https://www.zhihu.com/people/{}/".format(self.user_name),
+                                  headers=self.googlebot_header(),
                                   callback=self.parse_first_page_item)
+    def parse_jsons_item(self, response):
+        '''parse jsons after parsing first page json'''
+
+        jresp = json.loads(response.text)
+
+        json_url = jresp.get('paging')['next'] or None
+        data = jresp['data']
+        if data:
+            for i in range(len(data)):
+                if data[i]['verb'] != 'MEMBER_VOTEUP_ARTICLE':
+                    continue
+                item = ZhihuItem()
+
+                item['id'] = data[i]['target']['id']
+                item['action'] = data[i].get('action_text')
+                item['time'] = data[i].get('created_time')
+                item['content'] = data[i]['target'].get('title')
+                item['writer'] = data[i]['target']['author']['name']
+                # print(item)
+
+                yield item
+
+        if json_url:
+            yield scrapy.http.Request(json_url, headers=self.googlebot_header(),
+                                      callback=self.parse_jsons_item)
+        else:
+            raise Exception("zhong duan")
 
     def parse_first_page_item(self, response):
         # print(response.status_code)
         ''' main page crawling'''
         print("2")
         from bs4 import BeautifulSoup as bfs
-        print(response.text)
+        # print(response.text)
         soup = bfs(response.text, "lxml")
         j = soup.findAll('script', {"type": "text/json"})[1].string
         jj = json.loads(j)
@@ -47,7 +76,6 @@ class ZhihuSelfActSpider(CrawlSpider):
 
         def get_articles_fields(articles):
             res = []
-
             for key in list(articles.keys()):
                 tmp = {}
                 tmp['article'] = key
@@ -81,7 +109,7 @@ class ZhihuSelfActSpider(CrawlSpider):
                         "action": res[j].get("action"),
                         "createdTime": res[j].get("createdTime"),
                         "updatedTime": details[i].get("updated"),
-                        "author_name": details[i].get("author_name"),
+                        "authorName": details[i].get("author_name"),
                         "title": details[i].get("title"),
 
                     }
@@ -90,42 +118,17 @@ class ZhihuSelfActSpider(CrawlSpider):
             'createdTime'), reverse=True)
         for i in range(len(joined)):
             item = ZhihuItem()
-            #             id = scrapy.Field()
-            # url = scrapy.Field()
-            # name = scrapy.Field()
-            # action = scrapy.Field()
-            # writer = scrapy.Field()
-            # title = scrapy.Field()
-            # question = scrapy.Field()
-            # content = scrapy.Field()
-            # flag = scrapy.Field()
-            # time = scrapy.Field()
-
-            #     [{'article_id': '334325756',
-            #   'action': '赞同了文章',
-            #   'createdTime': 1607394299,
-            #   'updatedTime': 1607391682,
-            #   'author_name': 'Zilliz',
-            #   'title': 'Milvus 实战 | 基于 Milvus 的图像查重系统'},
             item['id'] = joined[i].get('article_id')
             item['action'] = joined[i].get('action')
             item['time'] = joined[i].get('createdTime')
             item['content'] = joined[i].get('title')
             item['writer'] = joined[i].get('author_name')
-            # item[''] = joined[i].get()
-            print(item)
 
             yield item
 
-        self.json_url = []
-
-
-        def parse_jsons_item(self, response):
-            '''parse jsons after parsing first page json'''
-            self.json_url = url.get('paging') or None
-
-            yield item
-
-            if self.json_url:
-                yield scrapy.http.Request(self.json_url, headers=self.googlebot_header(),
+        self.json_url = jj['initialState']['people']['activitiesByUser'].get(self.user_name)['next']
+        yield scrapy.http.Request(self.json_url,
+                                  headers=self.googlebot_header(),
                                   callback=self.parse_jsons_item)
+
+
